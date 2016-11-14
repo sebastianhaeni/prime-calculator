c17553a4bf678b0a37587aca3a6c32881d49f88b
Sebastian Häni <haeni.sebastian@gmail.com>
2016-11-14 22:20:57 +0100

    enable linux to build

--- build/build.js
+++ build/build.js
@@ -5,8 +5,11 @@
 const fs = require('fs');
 const childProcess = require('child_process');
 
+var isWin = /^win/.test(process.platform);
+var extension = isWin ? '.cmd' : '';
+
 // Generate static js resources
 console.log('Generating frontend...');
-childProcess.execFileSync('.\\node_modules\\.bin\\gulp.cmd');
+childProcess.execFileSync(`.${path.sep}node_modules${path.sep}.bin${path.sep}gulp${extension}`);
 
 console.log('Done!');
--- build/frontend.js
+++ build/frontend.js
@@ -2,9 +2,13 @@
  * Builds the frontend and starts a server with live reload.
  */
 const childProcess = require('child_process');
+const path = require('path');
+
+var isWin = /^win/.test(process.platform);
+var extension = isWin ? '.cmd' : '';
 
 console.log("Running app at http://localhost:3000");
-var child = childProcess.execFile('.\\node_modules\\.bin\\gulp.cmd', ['server'], function(error, stdout, stderr) {
+var child = childProcess.execFile(`.${path.sep}node_modules${path.sep}.bin${path.sep}gulp${extension}`, ['server'], function(error, stdout, stderr) {
     console.log(stdout);
 });
 child.stdout.on('data', function(data) {

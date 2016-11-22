e4a737e0f06943d5794b267da20c49f6bfbe6700
Sebastian Häni <haeni.sebastian@gmail.com>
2016-11-22 23:30:40 +0100

    beautification

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,19 +1,41 @@
 const config = require('./config');
 
+let results = [];
+$('[name=prime-number-candidate]').val(getRandom());
+let i = 0;
+
 $('#check-form').submit(() => {
-    var candidate = $('[name=prime-number-candidate]').val();
+    let candidate = $('[name=prime-number-candidate]').val();
+    $('[name=prime-number-candidate]').val(getRandom());
     if (!candidate) {
         alert('Please insert a number, com\'on.');
         return;
     }
 
-    $('#result').text('computing...');
+    results.push({n: candidate, number: 'computing...', i: ++i});
+    let index = i;
+    render();
 
     fetch(config.API + candidate)
         .then(response => response.json())
         .then(json => {
-            $('#result').text(json.result);
+            results = results.filter(result => result.n != candidate);
+            results.push({n: candidate, number: json.result, i: index});
+            render();
         });
 
     return false;
 });
+
+function render() {
+    $('#result').html('');
+    results.sort((a, b) => a.i - b.i);
+    let max = Math.max.apply(null, results.map(result => result.i));
+    results.filter(result => result.i > max - 10).forEach(result => {
+        $('#result').append(`<span class="nth">Prime ${result.n}:</span><span class="number">${result.number}</span><br>`);
+    });
+}
+
+function getRandom() {
+    return Math.floor((Math.random() * 100000) + 1);
+}
